b8dafd2ec877d322e74c5cb0ccced8ec45b7e68f
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-13 23:46:38 +0200

    update color seletion for transition pages

--- src/components/TransitionEffect.js
+++ src/components/TransitionEffect.js
@@ -5,7 +5,7 @@ const TransitionEffect = () => {
   return (
     <>
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-primary"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-dark dark:bg-light"
         initial={{ x: "100%", width: "100%" }}
         animate={{ x:"0%", width:"0%" }}
         exit={{x:["0%", "100%"], width:["0%","100%"]}}
