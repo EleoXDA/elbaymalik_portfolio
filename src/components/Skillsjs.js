0eeec7af4fb3ed851080ce58d22cb005468ee0bb
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-16 22:03:16 +0200

    update IT skills

--- src/components/Skillsjs.js
+++ src/components/Skillsjs.js
@@ -37,32 +37,24 @@ const Skills = () => {
       "
     >
    
-        <motion.div whileHover={{scale:0}} className="cursor-pointer flex rounded-full font-semibold bg-dark text-light p-0 shadow-dark
-        dark:bg-light dark:text-dark lg:p-0 md:p-0 xs:text-xs xs:p-0
+        <motion.div whileHover={{scale:1.05}} className="cursor-pointer flex rounded-full font-semibold bg-dark text-light p-8 shadow-dark
+        dark:bg-light dark:text-dark lg:p-6 md:p-4 xs:text-xs xs:p-2
         ">
-        
+        Web
       </motion.div>
 
         <Skill name="HTML" x="-20vw" y="2vw" />
-        <Skill name="CSS" x="-5vw" y="-10vw" />
-        <Skill name="JavaScript" x="25vw" y="6vw" />
-        <Skill name="ReactJS" x="0vw" y="12vw" />
-        <Skill name="NextJS" x="0vw" y="0vw" />
-        <Skill name="NuxtJS" x="15vw" y="-12vw" />
-        <Skill name="VueJS" x="-35vw" y="-5vw" />
-        <Skill name="Typescript" x="32vw" y="-5vw" />
-        <Skill name="Graphql" x="0vw" y="-20vw" />
-        <Skill name="Grafbase" x="-25vw" y="18vw" />
-        <Skill name="Tawilwind CSS" x="28vw" y="18vw" />
-        <Skill name="Sass" x="-20vw" y="-15vw" />
-        <Skill name="SCSS" x="-12vw" y="8vw" />
-        <Skill name="PHP" x="13vw" y="10vw" />
-        <Skill name="Ruby on Rails" x="-30vw" y="10vw" />
-        <Skill name="Figma" x="-33vw" y="2vw" />
-        <Skill name="Adobe XD" x="-15vw" y="-5vw" />
-        <Skill name="Flutter" x="12vw" y="-5vw" />
-        <Skill name="Dart" x="12vw" y="-18vw" />
-        <Skill name="Postman" x="25vw" y="-20vw" />        
+
+          <Skill name="CSS" x="-5vw" y="-10vw" />
+          <Skill name="JavaScript" x="20vw" y="6vw" />
+          <Skill name="ReactJS" x="0vw" y="12vw" />
+          <Skill name="NextJS" x="-20vw" y="-15vw" />
+          <Skill name="GatsbyJS" x="15vw" y="-12vw" />
+          <Skill name="Github" x="-35vw" y="-5vw" />
+          <Skill name="Web Design" x="32vw" y="-5vw" />
+          <Skill name="Figma" x="0vw" y="-20vw" />
+          <Skill name="Firebase" x="-25vw" y="18vw" />
+          <Skill name="Tawilwind CSS" x="28vw" y="18vw" />
     </div></>
   );
 };
