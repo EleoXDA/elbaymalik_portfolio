a74a62ab63e75e805b995de8e425acd63624bd05
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-16 23:55:48 +0200

    update text for better personalization

--- src/pages/about.js
+++ src/pages/about.js
@@ -49,7 +49,7 @@ export default function About() {
       >
         <Layout className="pt-16">
           <AnimatedText
-            text="Passion Fuels Purpose!"
+            text="Driven by Passion, Defined by Purpose!"
             className="mb-16 !text-8xl !leading-tight lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"
           />
 
