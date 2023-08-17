b132e47ff147ad2896d0ff384d09694b2e88df6c
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-17 18:20:58 +0200

    add contact form button to homescreen

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,14 @@ import Link from "next/link";
 import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg";
 import profilePic from "../../public/images/profile/developer-pic-1.png";
 import TransitionEffect from "@/components/TransitionEffect";
+import { useState } from 'react';
+import { ContactForm } from "@/components/contactForm";
+
 
 
 export default function Home() {
-  
+  const [showModal, setShowModal] = useState(false); 
+
   return (
     <>
       <Head>
@@ -47,28 +51,25 @@ export default function Home() {
               </p>
               <div className="mt-2 flex items-center self-start lg:self-center">
                 <Link
-                  // whileHover={{
-                  //   cursor: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='40' height='48' viewport='0 0 100 100' style='font-size:24px;'><text y='50%'>👆</text></svg>"), auto`,
-                  // }}
                   href="https://drive.google.com/file/d/1oJ9H7wGFC-Ix0OQwowjJqNOquIBNtO_V/view?usp=sharing"
                   target={"_blank"}
                   className={`flex items-center rounded-lg border-2 border-solid bg-dark p-2.5 px-6 text-lg font-semibold
-            capitalize text-light hover:border-dark hover:bg-transparent hover:text-dark 
-            dark:bg-light dark:text-dark dark:hover:border-light dark:hover:bg-dark dark:hover:text-light
-            md:p-2 md:px-4 md:text-base
+                              capitalize text-light hover:border-dark hover:bg-transparent hover:text-dark 
+                              dark:bg-light dark:text-dark dark:hover:border-light dark:hover:bg-dark dark:hover:text-light
+                              md:p-2 md:px-4 md:text-base
              `}
                   download
                 >
                   Resume <LinkArrow className="ml-1 !w-6 md:!w-4" />
                 </Link>
 
-                <Link
-                  href="mailto:elbay.malik@gmail.com"
+                <button
                   className="ml-4 text-lg font-medium capitalize text-dark underline 
                   dark:text-light md:text-base"
+                  onClick={() => setShowModal(true)}
                 >
                   Contact
-                </Link>
+                </button>
               </div>
             </div>
           </div>
@@ -82,6 +83,11 @@ export default function Home() {
             alt=""
           />
         </div>
+        {showModal && (
+          <div className="fixed inset-0 flex items-center justify-center z-50">
+              <ContactForm closeModal={() => setShowModal(false)} />
+          </div>
+        )}
       </article>
     </>
   );
