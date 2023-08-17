d96fb50b587093bd7b0108cf1599bdbd81d63c56
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-17 13:34:46 +0200

    spelling issue fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
                 className="!text-left !text-6xl xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
               <p className="my-4 text-base font-medium md:text-sm sm:!text-xs">
-              As a skilled full-stack developer, I am dedicated to turning ideas into innovative web or mobile applications. Explore my latest projects, showcasing my expertise in fullstackk web development.
+              As a skilled full-stack developer, I am dedicated to turning ideas into innovative web or mobile applications. Explore my latest projects, showcasing my expertise in fullstack web development.
               </p>
               <div className="mt-2 flex items-center self-start lg:self-center">
                 <Link
