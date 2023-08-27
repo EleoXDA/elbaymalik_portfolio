aef4ded585ad936aecb611ff97d9bafc1b2177c3
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-28 01:27:38 +0200

    update information about my education

--- src/components/Education.js
+++ src/components/Education.js
@@ -44,24 +44,52 @@ const Education = () => {
         />
         <ul className="w-full flex flex-col items-start justify-between ml-4">
           <Details
-            type="Bachelor of Science in Biology"
-            time="2008-2013"
-            place="Middle East Technical University (METU)"
-            info="Basics of scientific research and introduction to general scientific applications."
+            type="Web Development Bootcamp Certification"
+            time="2022"
+            place="Le Wagon Web Development Bootcamp"
+            info={[
+              "• 9-week instensive full-time Coding Bootcamp that focused on Ruby on Rails, HTML, CSS, JavaScript, React, Redux, SQL, Git, GitHub, Heroku, and pair programming.", 
+              "• Two weeks of individual work on a final project, which was a full-stack web application built with Ruby on Rails, HTML, CSS, JavaScript, and PostgreSQL.",
+              "• Final project included starting from sketches, wireframes (Figma), and user stories to a deployed web application with a database and a user interface."
+            ].map((line, index) => (
+              <React.Fragment key={index}>
+                {line}
+                <br/>
+              </React.Fragment>
+            ))}
           />
-
           <Details
             type="Master of Science in Micro and Nanotechnology"
             time="2014-2017"
             place="Middle East Technical University (METU)"
-            info="Development of silicon chips and MEMS devices for automated testing."
+            info={[
+              "• Development of 3D wet-spun PCL scaffolds for bone tissue engineering", 
+              "• Addition of hydroxyapatite to improve mechanical properties and bioactivity",
+              "• Surface modification with AAc and gelatin to improve cell adhesion and proliferation",
+              "• Gelatin microvesicles were added for controlled release of antibacterial agents",
+              "• Characterization of scaffolds with SEM, XPS, and mechanical tester",
+              "• Cell culture and cell viability tests with Saos-2 osteosarcoma cell liness",
+            ].map((line, index) => (
+              <React.Fragment key={index}>
+                {line}
+                <br/>
+              </React.Fragment>
+            ))}
           />
-
           <Details
-            type="Web Development Bootcamp Certification"
-            time="2022"
-            place="Le Wagon Web Development Bootcamp"
-            info="9-week full time intensive coding bootcamp learning HTML, CSS, Bootstrap, JavaScript ES6, SQL, Git, GitHub, Heroku and Ruby on Rails."
+            type="Bachelor of Science in Biology"
+            time="2008-2013"
+            place="Middle East Technical University (METU)"
+            info={[
+              "• Basics of Scientific Research and Presentation", 
+              "• Lab internship in the field of Molecular Biology",
+              "• Biomaterials and Tissue Engineering courses for Master studies",
+            ].map((line, index) => (
+              <React.Fragment key={index}>
+                {line}
+                <br/>
+              </React.Fragment>
+            ))}
           />
         </ul>
       </div>
