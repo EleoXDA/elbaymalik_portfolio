7458027ea72bd8361b76d0bd756b63fb12a837bd
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-29 23:29:14 +0200

    reorganize tailwind classes for visibility

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,8 +13,9 @@ const Skill = ({ name, x, y }) => {
       viewport={{ once: true }}
       className="cursor-pointer w-max origin-center absolute font-bold 
        font-semibold bg-dark text-light py-3 px-6 rounded-full dark:bg-light dark:text-dark
-       4xl:text-2xl 4xl:px-9 4xl:py-11 3xl:text-2xl 3xl:py-8 3xl:px-10 2xl:text-2xl 2xl:py-6 2xl:px-8 xl:py-4 xl:px-6 lg:py-3 lg:px-5 md:text-sm md:py-2 md:px-4  
-       xs:py-0.5 xs:px-1
+       4xl:text-2xl 4xl:px-9 4xl:py-11 3xl:text-2xl 3xl:py-8 3xl:px-10 
+       2xl:text-2xl 2xl:py-6 2xl:px-8 xl:py-4 xl:px-6 lg:py-3 lg:px-5 
+       md:text-sm md:py-2 md:px-4 xs:py-0.5 xs:px-1
        "
     >
       {name}
@@ -42,27 +43,26 @@ const Skills = () => {
         
       </motion.div>
 
-      <Skill name="HTML" x="10vw" y="-20vw" />
-      <Skill name="CSS" x="32vw" y="-5vw" />
-      <Skill name="JavaScript" x="10vw" y="-5vw" />
-      <Skill name="ReactJS" x="15vw" y="-12vw" />
-      <Skill name="NextJS" x="32vw" y="-12.5vw" />
-      <Skill name="NuxtJS" x="-35vw" y="-5vw" />
+      <Skill name="HTML" x="30vw" y="2vw" />
+      <Skill name="CSS" x="40vw" y="-5vw" />
+      <Skill name="JavaScript" x="8vw" y="-1vw" />
+      <Skill name="ReactJS" x="6vw" y="-12vw" />
+      <Skill name="NextJS" x="38vw" y="-12.5vw" />
+      <Skill name="NuxtJS" x="23vw" y="-8vw" />
       <Skill name="VueJS" x="33vw" y="12vw" />
-      <Skill name="Typescript" x="3vw" y="2vw" />
-      <Skill name="Graphql" x="-16vw" y="3.5vw" />
-      <Skill name="Grafbase" x="-28vw" y="-20vw" />
-      <Skill name="Tailwind CSS" x="25vw" y="3vw" />
-      <Skill name="Sass" x="0vw" y="12vw" />
-      <Skill name="SCSS" x="25vw" y="-20vw" />
-      <Skill name="PHP" x="-33vw" y="3vw" />
-      <Skill name="Ruby on Rails" x="-30vw" y="12vw" />
-      <Skill name="Figma" x="-15vw" y="-5vw" />
-      <Skill name="Adobe XD" x="-7vw" y="-20vw" />
-      <Skill name="Flutter" x="17vw" y="11vw" />
-      <Skill name="Dart" x="-31vw" y="-12.5vw" />
-      <Skill name="Postman" x="-12vw" y="-12vw" />
-    
+      <Skill name="Typescript" x="13vw" y="10vw" />
+      <Skill name="Graphql" x="-11vw" y="3.5vw" />
+      <Skill name="Grafbase" x="-34vw" y="-20vw" />
+      <Skill name="Tailwind.CSS" x="20vw" y="-20vw" />
+      <Skill name="Sass" x="0vw" y="-20vw" />
+      <Skill name="SCSS" x="39vw" y="-20vw" />
+      <Skill name="PHP" x="-14vw" y="-4vw" />
+      <Skill name="Dart" x="-36vw" y="12vw" />
+      <Skill name="Figma" x="-15vw" y="-20vw" />
+      <Skill name="Adobe XD" x="-34vw" y="-10vw" />
+      <Skill name="Flutter" x="-17vw" y="12vw" />
+      <Skill name="Ruby on Rails" x="-32vw" y="0vw" />
+      <Skill name="Postman" x="-12vw" y="-12vw" />    
     </div></>
   );
 };
